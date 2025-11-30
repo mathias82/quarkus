48c03851ad1da117bb01b2ce842683cfda571198
matthaios.stavrou <matthaios.stavrou@accenture.com>
2025-11-30 14:24:02 +0200

    Dev mode: always return 500 instead of 404 when deployment fails

--- extensions/vertx-http/runtime/src/main/java/io/quarkus/vertx/http/runtime/VertxHttpRecorder.java
+++ extensions/vertx-http/runtime/src/main/java/io/quarkus/vertx/http/runtime/VertxHttpRecorder.java
@@ -300,6 +300,13 @@ public class VertxHttpRecorder {
                 router.route().order(RouteConstants.ROUTE_ORDER_HOT_REPLACEMENT).blockingHandler(hotReplacementHandler);
             }
 
+            router.route().last().handler(rc -> rc.response()
+                    .setStatusCode(500)
+                    .putHeader("Content-Type", "text/plain")
+                    .end("Dev mode startup failed – Application deployment did not complete successfully."));
+
+            rootHandler = router;
+
             Handler<HttpServerRequest> root = router;
             LiveReloadConfig liveReloadConfig = config.getConfigMapping(LiveReloadConfig.class);
             if (liveReloadConfig.password().isPresent()
